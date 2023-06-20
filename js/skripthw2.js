b7587a6919741bac90fdbace0bbd8b12b64b0631
DenisChornui <129185799+DenisChornui@users.noreply.github.com>
2023-06-20 16:56:06 +0300

    hw2

---
+++ js/skripthw2.js
@@ -0,0 +1,236 @@
+// Масиви
+// const clients = ["Mango", "Poly", "Ajax"];
+
+
+// // Зазначаючи в дужках індекс елемента, ми отримуємо його значення
+// console.log(clients[0]); // Mango
+// console.log(clients[1]); // Poly
+// console.log(clients[2]); // Ajax
+
+// const clients = ["Mango", "Poly", "Ajax"];
+// clients[0] = "Kiwi";
+// clients[1] = "Pango";
+// console.log(clients); // ["Kiwi", "Pango", "Ajax"]
+
+// const clients = ["Mango", "Poly", "Ajax"];
+// const lastElementIndex = clients.length - 1;
+// console.log(lastElementIndex); // 2
+// console.log(clients[lastElementIndex]); // "Ajax"
+
+// Інтерація по масиву
+// const clients = ["Mango", "Ajax", "Poly"];
+
+// for (let i = 0; i < clients.length; i += 1) {
+//   console.log(clients[i]);
+// }
+
+// const clients = ["Mango", "Ajax", "Poly"];
+
+// for (const client of clients) {
+//   console.log(client);
+// }
+
+// const string = "javascript";
+
+// for (const character of string) {
+//   console.log(character);
+// }
+
+// const clients = ["Mango", "Poly", "Ajax"];
+// const clientNameToFind = "Poly";
+// let message;
+
+// for (const client of clients) {
+//   // На кожній ітерації будемо перевіряти чи збігається елемент масиву з
+//   // іменем клієнта. Якщо збігається - записуємо в message повідомлення
+//   // про успіх і робимо break, щоб далі не шукати
+//   if (client === clientNameToFind) {
+//     message = "Клієнт з таким ім'ям є в базі даних!";
+//     break;
+//   }
+
+//   // Якщо вони не збігаються - записуємо в message повідомлення про відсутність імені
+//   message = "Клієнт з таким ім'ям відсутній в базі даних!";
+// }
+
+// console.log(message); // "Клієнт з таким ім'ям є в базі даних!"
+
+// const clients = ["Mango", "Poly", "Ajax"];
+// const clientNameToFind = "Poly";
+// let message = "Клієнт з таким ім'ям відсутній в базі даних!";
+
+// for (const client of clients) {
+//   if (client === clientNameToFind) {
+//     message = "Клієнт з таким ім'ям є в базі даних!";
+//     break;
+//   }
+//   // Якщо не збігається, то на цій ітерації нічого не робимо
+// }
+
+// console.log(message); // Клієнт з таким ім'ям є в базі даних!
+
+// const numbers = [1, 3, 14, 18, 4, 7, 29, 6, 34];
+// const threshold = 15;
+
+// // Для чисел, менших ніж порогове значення, спрацьовує continue, виконання тіла
+// // припиняється і управління передається на наступну ітерацію.
+// for (let i = 0; i < numbers.length; i += 1) {
+//   if (numbers[i] < threshold) {
+//     continue;
+//   }
+
+//   console.log(`Число більше за ${threshold}: ${numbers[i]}`); // 18, 29, 34
+// }
+
+// let a = 5;
+// // Присвоєння за значенням, в пам'яті буде створена ще
+// // одна комірка, в яку буде скопійоване значення 5
+// let b = a;
+// console.log(a); // 5
+// console.log(b); // 5
+
+// // Змінимо значення a
+// a = 10;
+// console.log(a); // 10
+// // Значення b не змінилося, оскільки це окрема копія
+// console.log(b); // 5
+
+// const a = ["Mango"];
+// // Оскільки a - це масив, в b записується посилання на вже існуючий
+// // масив в пам'яті. Тепер a і b вказують на той самий масив.
+// const b = a;
+// console.log(a); // ["Mango"]
+// console.log(b); // ["Mango"]
+
+// // Змінимо масив, додавши ще один елемент, використовуючи вказівник з a
+// a.push("Poly");
+// console.log(a); // ["Mango", "Poly"]
+
+// // b також змінилось, тому що b, як і a,
+// // просто містить посилання на те ж саме місце в пам'яті
+// console.log(b); // ["Mango", "Poly"]
+
+// // Результат повторюється
+// b.push("Ajax");
+// console.log(a); // ["Mango", "Poly", "Ajax"]
+// console.log(b); // ["Mango", "Poly", "Ajax"]
+
+// Методи масиву
+// const name = "Mango";
+// console.log(name.split("")); // ["M", "a", "n", "g", "o"]
+
+// const message = "JavaScript - це цікаво";
+// console.log(message.split(" ")); // ["JavaScript", "-", "це", "цікаво"]
+
+// const words = ["JavaScript", "це", "цікаво"];
+// console.log(words.join("")); // "JavaScriptцецікаво"
+// console.log(words.join(" ")); // "JavaScript це цікаво"
+// console.log(words.join("-")); // "JavaScript-це-цікаво"
+
+// const clients = ["Mango", "Ajax", "Poly", "Kiwi"];
+// console.log(clients.indexOf("Poly")); // 2
+// console.log(clients.indexOf("Monkong")); // -1
+
+// const clients = ["Mango", "Ajax", "Poly", "Kiwi"];
+// console.log(clients.includes("Poly")); // true
+// console.log(clients.includes("Monkong")); // false
+
+// Виносимо варіанти в масив
+// const redFruits = ["apple", "strawberry", "cherry", "cranberries"];
+// const fruit = "cherry";
+// // Перевіряємо присутність елемента
+// const hasFruit = redFruits.includes(fruit);
+
+// if (hasFruit) {
+//   console.log(`${fruit} is a red fruit!`);
+// }
+
+// const numbers = [];
+
+// numbers.push(1);
+// console.log(numbers); // [1]
+
+// numbers.push(2);
+// console.log(numbers); // [1, 2]
+
+// numbers.push(3);
+// console.log(numbers); // [1, 2, 3]
+
+// numbers.push(4);
+// console.log(numbers); // [1, 2, 3, 4]
+
+// numbers.push(5);
+// console.log(numbers); // [1, 2, 3, 4, 5]
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// console.log(numbers.pop()); //  5
+// console.log(numbers); // [1, 2, 3, 4]
+
+// console.log(numbers.pop()); //  4
+// console.log(numbers); // [1, 2, 3]
+
+// console.log(numbers.pop()); //  3
+// console.log(numbers); // [1, 2]
+
+// console.log(numbers.pop()); //  2
+// console.log(numbers); // [1]
+
+// console.log(numbers.pop()); //  1
+// console.log(numbers); // []
+
+// const clients = ["Mango", "Ajax", "Poly", "Kiwi"];
+// console.log(clients.slice(1, 3)); // ["Ajax", "Poly"]
+
+// const clients = ["Mango", "Ajax", "Poly", "Kiwi"];
+// console.log(clients.slice()); // ["Mango", Ajax", "Poly", "Kiwi"]
+
+// const clients = ["Mango", "Ajax", "Poly", "Kiwi"];
+// console.log(clients.slice(1)); // ["Ajax", "Poly", "Kiwi"]
+// console.log(clients.slice(2)); // ["Poly", "Kiwi"]
+
+// const clients = ["Mango", "Ajax", "Poly", "Kiwi"];
+// console.log(clients.slice(-2)); // ["Poly", "Kiwi"]
+
+// const scores = [1, 2, 3, 4, 5];
+
+// // Видаляємо три елементи масиву, починаючи з першого елемента (індекс 0)
+// const deletedScores = scores.splice(0, 3);
+
+// // Тепер масив scores містить два елементи
+// console.log(scores); // [4, 5]
+
+// // А масив deletedScores містить три видалені елементи
+// console.log(deletedScores); // [1, 2, 3]
+
+// const colors = ["red", "green", "blue"];
+
+// colors.splice(2, 0, "purple");
+// console.log(colors); // ["red", "green", "purple", "blue"]
+
+// const colors = ["red", "green", "blue"];
+
+// colors.splice(1, 0, "yellow", "pink");
+// console.log(colors); // ["red", "yellow", "pink", "green", "blue"]
+
+// const languages = ["C", "C++", "Java", "JavaScript"];
+
+// // Заміняємо елемент з індексом 1 на новий
+// languages.splice(1, 1, "Python");
+// console.log(languages); // ["C", "Python", "Java", "JavaScript"]
+
+// // Заміняємо один елемент (з індексом 2) на декілька
+// languages.splice(2, 1, "C#", "Swift", "Go");
+// console.log(languages); // ["C", "Python", "C#", "Swift", "Go", "JavaScript"]
+
+// const oldClients = ["Mango", "Ajax", "Poly", "Kiwi"];
+// const newClients = ["Monkong", "Singu"];
+
+// const allClientsWithOldFirst = oldClients.concat(newClients);
+// console.log(allClientsWithOldFirst); // ["Mango", "Ajax", "Poly", "Kiwi", "Monkong", "Singu"]
+
+// const allClientsWithNewFirst = newClients.concat(oldClients);
+// console.log(allClientsWithNewFirst); // ["Monkong", "Singu", "Mango", "Ajax", "Poly", "Kiwi"]
+
+// console.log(oldClients); // ["Mango", "Ajax", "Poly", "Kiwi"]
+// console.log(newClients); // ["Monkong", "Singu"]
