2d40119693f2c4b0a891324484df62a951a5de82
DenisChornui <129185799+DenisChornui@users.noreply.github.com>
2023-07-21 15:33:28 +0300

    hw5

--- js/skripthw4.js
+++ js/skripthw4.js
@@ -792,3 +792,56 @@
 // const oddNumbers = numbers.filter(number => number % 2 !== 0);
 // console.log(evenNumbers)
 // console.log(oddNumbers)
+
+// 20 task
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     genres: ["adventure", "history"],
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     genres: ["fiction", "mysticism"],
+//   },
+//   {
+//     title: "Redder Than Blood",
+//     author: "Tanith Lee",
+//     genres: ["horror", "mysticism", "adventure"],
+//   },
+// ];
+// // Change code below this line
+// const allGenres = books.flatMap(book => book.genres);
+// console.log(allGenres)
+// const uniqueGenres = allGenres.filter((book, idx, arr) => arr.indexOf(book) === idx);
+// console.log(uniqueGenres)
+
+// 21 task
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    rating: 8.38,
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    rating: 8.51,
+  },
+  {
+    title: "The Dream of a Ridiculous Man",
+    author: "Fyodor Dostoevsky",
+    rating: 7.75,
+  },
+  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+  { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+];
+
+const MIN_RATING = 8;
+const AUTHOR = "Bernard Cornwell";
+// Change code below this line
+
+const topRatedBooks = books;
+const booksByAuthor = books.filter((book, idx, arr) => arr.indexOf(book) === AUTHOR);
+console.log(booksByAuthor)
